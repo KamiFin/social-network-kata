5e4b36bd3f8a64e634c85ace3722f92abead0997
KamiFin <rigamonti.filippo@gmail.com>
2022-06-13 18:13:24 +0200

    use only test

--- ts/src/app.ts
+++ ts/src/app.ts
@@ -1,17 +1,7 @@
 "use strict";
 
-import { User } from "./classes/User";
-
-function aliceWriteAPost() {
-  const alice = new User("alice", "Alice", "ali@kata.com");
-  
-  // Write a post to timeline
-  alice.writePost("First post of Alice");
-  console.log(alice.getTimeline());
-}
-
 function main() {
-  aliceWriteAPost();
+  console.log("WARNING: please run 'npm run test' command");
 }
 
 main();
